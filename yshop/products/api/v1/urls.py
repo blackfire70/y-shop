489de2c69d27f7caac38baaa3098d616dd5ae8b3
Miguel Concepcion <miguel.concepcion@outlook.com>
2020-10-27 23:52:45 +0800

    add urls for product category viewset

--- yshop/products/api/v1/urls.py
+++ yshop/products/api/v1/urls.py
@@ -2,10 +2,12 @@ from django.urls import include, path
 
 from rest_framework import routers
 
+from products.api.v1.views import ProductCategoryViewSet
 from products.api.v1.views import ProductViewSet
 
 router = routers.DefaultRouter()
 router.register(r'products', ProductViewSet)
+router.register(r'product-categories', ProductCategoryViewSet)
 
 urlpatterns = [
     path('', include(router.urls))
